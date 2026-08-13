71e045b14e1a25ae05908b311e69b9b098b7090a
StrategyandDesign <Micahdcanfield@gmail.com>
2026-08-13 01:25:18 -0500

    Revert ungate from #62; that work moves to a follow-up PR

--- supabase/migrations/20260813_org_participation_invoker.sql
+++
@@ -1,4 +0,0 @@
--- org_participation was a security-definer-style view (reloptions null on prod).
--- GRANT SELECT TO authenticated therefore bypassed seats RLS and could leak
--- other orgs' emails. Invoker mode honors seats RLS: org leaders see their org only.
-alter view public.org_participation set (security_invoker = true);
